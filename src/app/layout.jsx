a389fc254955d2786bcda0819a89d65b141bbcdb
Naeem Sarker <naeemsarker85@gmail.com>
2025-04-17 12:23:55 +0600

    feat: setup and added meta title, favicon

---
+++ src/app/layout.jsx
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "DingDing - A Social Platform for Young Generation",
+  description: "DingDing is a social platform for the young generation.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
